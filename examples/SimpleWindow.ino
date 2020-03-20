c5513466bc612860e181b8563b871a4f784182be
Gabriele266 <62379767+Gabriele266@users.noreply.github.com>
2020-03-20 16:31:33 +0100

    Create SimpleWindow.ino

---
+++ examples/SimpleWindow.ino
@@ -0,0 +1,34 @@
+/*
+ * Autore: Cavallo Gabriele
+ * Descrizione: File di esempio per la creazione di una finestra con schermo lcd i2c
+ * 
+ * Il programma crea un oggetto liquidrcystal i2c, una finestra e la mostra
+ */
+#include <Arducore.h>
+#include <LiquidCrystal_I2C.h>
+
+// Creo la finestra
+Window *win = new Window("Nome", "Ciao");
+
+// Creo l'oggetto I2c
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup() {
+  // Inizializzo lo schermo
+  lcd.init();
+  lcd.backlight();
+
+
+  // Imposto la superficie per il disegno
+  win->setSurface(&lcd);
+
+  // Disegno la finestra
+  win->draw();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
