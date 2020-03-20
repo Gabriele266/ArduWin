d58a4121c4c8a3b5b5dfbbc6235b9d55a365efb4
Gabriele266 <62379767+Gabriele266@users.noreply.github.com>
2020-03-20 16:39:37 +0100

    Update empty_icon.cpp

--- icons/empty_icon.cpp
+++ icons/empty_icon.cpp
@@ -1,24 +1,22 @@
 /*
 	Autore: Cavallo Gabriele
-	Contenuto: Contiene una icona che rappresenta un orologio
+	Contenuto: icona vuota per l'utilizzo nella libreria
 	Licenza: GPL
 */
-#ifndef CLOCK_ICON_INCLUDED
-#define CLOCK_ICON_INCLUDED
 
-// Header di Arduino
-#include <Arduino.h>
+#ifndef EMPTY_ICON_INCLUDED
+#define EMPTY_ICON_INCLUDED
 
-// Rappresentazione dell' icona in un array di 8 elementi di byte
-static byte clock_icon[8]{
-     B00000,
-     B01110,
-     B10001,
-     B10101,
-     B10101,
-     B10001,
-     B01110,
-     B00000
+// Rappresenta una icona vuota
+static byte empty_icon[8] = {
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000
 };
 
 #endif
