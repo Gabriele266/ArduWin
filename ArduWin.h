6d74e8787b8ae56434504f41a8e3115de0100191
Gabriele266 <62379767+Gabriele266@users.noreply.github.com>
2020-03-21 12:18:58 +0100

    Add files via upload

---
+++ ArduWin.h
@@ -0,0 +1,31 @@
+/*
+========================================================
+========================================================
+File principale del Framework ArduWin
+Questo framework serve per gestire l'interfaccia visiva su schermi lcd di arduino
+Licenza: GPL
+La licenza è valida su tutti i sorgenti del progetto.
+========================================================
+========================================================
+*/
+// Espone la versione del framework
+#define ARDUWIN_VERSION "1.1"
+
+// HEADER PER LE FINESTRE
+#include "GWindow.h"
+// HEADER PER LE LISTE DI FINESTRE
+#include "GWinList.h"
+// HEADER PER I CONTROLLI
+#include "GControl.h"
+// Header per le etichette
+#include "GLabel.h"
+// Header per itipi di dato
+#include "location.h"
+// Header per i pulsanti
+#include "GButton.h"
+// Header per gli eventi
+#include "GEvent.h"
+// Header per le icone
+#include "GIcon.h"
+// Header con le icone predefinite
+#include "default_icons.h"
