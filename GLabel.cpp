9e6c43ccdb95814389d7df997d0022144b3480f9
Gabriele <gabri.cabal@gmail.com>
2020-10-20 21:13:07 +0200

    Miglioramento e stabilizzazione GLabel

--- GLabel.cpp
+++ GLabel.cpp
@@ -4,25 +4,33 @@
 #include "GLabel.h"
 
 GLabel::GLabel(){
-	
+	icon = nullptr;
 }
 
-GLabel::GLabel(char name[], char text[]){
+GLabel::GLabel(char name[], char text[]) : GLabel(){
 	setName(name);
 	setText(text);
 }
 
-GLabel::GLabel(char name[], char text[], location l){
+GLabel::GLabel(char name[], char text[], location l) : GLabel(){
 	setName(name);
 	setText(text);
 	setLocation(l);
 }
 
-void GLabel::begin() {
-	// Controllo che l'icona esista
-	if (icon != nullptr && !icon->isEmpty()) {
-		__surf->createChar(icon->getIndex(), icon->getCode());
-	}
+bool GLabel::begin() {
+    // controllo che esista la superficie
+    if(getSurface() != nullptr){
+        // Controllo che l'icona esista
+        if (icon != nullptr && !icon->isEmpty()) {
+            __surf->createChar(icon->getIndex(), icon->getCode());
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    return false;
 }
 
 void GLabel::setIcon(GIcon* icn) {
@@ -30,21 +38,25 @@ void GLabel::setIcon(GIcon* icn) {
 }
 
 bool GLabel::draw(){
-	// Tolgo il blink
-	__surf->noBlink();
-	// Imposto il cursore
-	__surf->setCursor(getLocation().x, getLocation().y);
-	// Controllo se devo mettere una icona
-	if (icon != nullptr && !icon->isEmpty()) {
-		// Disegno l'icona
-		__surf->write(byte(icon->getIndex()));
-	}
-	// Disegno il testo
-	__surf->print(__text);
+    // controllo che esista la superficie
+    if(getSurface() != nullptr){
+        // Tolgo il blink
+        __surf->noBlink();
+        // Imposto il cursore
+        __surf->setCursor(getLocation().x, getLocation().y);
+        // Controllo se devo mettere una icona
+        if (icon != nullptr && !icon->isEmpty()) {
+            // Disegno l'icona
+            __surf->write(byte(icon->getIndex()));
+        }
+        // Disegno il testo
+        __surf->print(__text);
+        return true;
+    }
+    return false;
 }
 
 void GLabel::updateEvents(location l){
 	// Non ci sono eventi
-	
 }
 #endif
--- GLabel.h
+++ GLabel.h
@@ -1,40 +1,44 @@
 /*
       Autore: Cavallo Gabriele
-      Versione: 1.1
-      Tipologia licenza: GPL
-      Descrizione licenza: GPL
-      Nome classe: Label
       Descrizione classe: Rappresenta una etichetta in una interfaccia lcd
       Progetto: ArduWin
 */
+
 #include "GControl.h"
-#include <arduino.h>
+#include <Arduino.h>
 #include "GIcon.h"
 #include "location.h"
 
 #ifndef LABEL_H
 #define LABEL_H
 
+/// Rappresenta una etichetta in ArduWin
 class GLabel : public GControl{
 public:
-	// Contruttore vuoto
+	/// Contruttore vuoto
     GLabel();
-	// Costruttore che imposta il nome e il testo
+
+	/// Costruttore che imposta il nome e il testo
 	GLabel(char name[], char text[]);
-	// Construttore che imposta il nome il testo e la posizione
+
+	/// Construttore che imposta il nome il testo e la posizione
 	GLabel(char name[], char text[], location loc);
 
-	// Funzione per mostrarlo
-	// Ridefinizione del metodo virtuale
+	/// Funzione per mostrarlo
+	/// Ridefinizione del metodo virtuale
 	bool draw();
-	// Inizializza l'icona impostata
-	void begin();
+
+	/// Inizializza l'icona impostata
+	bool begin();
+
+	/// Aggiorna gli eventi
 	void updateEvents(location l);
 
+    /// Imposta l'icona della etichetta
 	void setIcon(GIcon *icn);
 private:
 	// Punta all' icona
-	GIcon *icon;
+	GIcon *icon = nullptr;
 };
 
 #endif
