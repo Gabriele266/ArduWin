14489e9c29f76da9ebf7de0032e8ef6a1fa09ee4
Gabriele <gabri.cabal@gmail.com>
2020-10-20 16:03:55 +0200

    Aggiunta commenti documentazione e nullptr per handler eventi

--- GControl.h
+++ GControl.h
@@ -28,25 +28,29 @@ class GControl{
       public:
           // Costruttore di default
 		GControl();
-
+        /// Costruttore di base con nome e testo
 		GControl(char name[], char text[]);
-        // Costruttore che accetta un
+        /// Costruttore che accetta un
         GControl(char name[], char text[], location l);
-		// Ottiene o imposta il nome del GControllo
+		/// Ottiene o imposta il nome del GControllo
 		void setName(char _n[]);
+		/// Restituisce il nome del controllo
 		char* getName();
 
-		// Ottiene o imposta il testo
+		/// Ottiene o imposta il testo
 		void setText(char _t[]);
 		char* getText();
 
-		// Ottiene o imposta i tag del GControllo
+		/// Ottiene o imposta i tag del GControllo
 		void setTags(char _t[]);
+		/// Restituisce i tag associati al controllo
 		char* getTags();
 
-		// Ottiene o imposta la posizione del GControllo
+		/// Ottiene o imposta la posizione del GControllo
 		void setLocation(location l);
+		/// Imposta la posizione in base ad una coppia di coordinate
 		void setLocation(int x, int y);
+		/// Restituisce la posizione del controllo
 		location getLocation();
 
 		// Ottiene o imposta la surface su cui disegnare
@@ -61,28 +65,28 @@ class GControl{
 		LiquidCrystal* getSurface();
 		#endif
 
-		// Disegna il GControllo
+		/// Disegna il GControllo
 		virtual bool draw();
-		// Restituisce lo stato del GControllo
+		/// Restituisce lo stato del GControllo
 		bool getShownState();
 
-		// Imposta il gestore degli eventi
+		/// Imposta il gestore degli eventi
 		void setEventHandler(void (*ptr)(GEvent *event));
-		// CHiama l'handler
+		/// CHiama l'handler
 		void callHandler(GEvent *event);
 
-		// Funzione per aggiornare gli eventi
+		/// Funzione per aggiornare gli eventi
 		virtual void updateEvents(location l);
 
-		// Nome del controllo
+		/// Nome del controllo
 		char __name[10];
-		// Testo associato al controllo
+		/// Testo associato al controllo
 		char __text[10];
-		// Tag del controllo
+		/// Tag del controllo
 		char __tags[10];
-		// Posizione del controllo nella interfaccia
+		/// Posizione del controllo nella interfaccia
 		location __point;
-		// Surface per il disegno
+		/// Surface per il disegno
 		#if defined ARDUWIN_USE_I2C
 		LiquidCrystal_I2C *__surf = nullptr;
 		#else
@@ -90,10 +94,10 @@ class GControl{
 		LiquidCrystal *__surf = nullptr;
 		#endif
 
-		// Definisce se l'oggetto è mostrato
+		/// Definisce se l'oggetto è mostrato
 		bool __isShown = false;
-		// Puntatore a una funzione che gestisce gli eventi
-		void (*eventHandler)(GEvent *event);
+		/// Puntatore a una funzione che gestisce gli eventi
+		void (*eventHandler)(GEvent *event) = nullptr;
     private:
 
 
