112ac0d94800262d4f6b6f6f510124cac3555c6b
Gabriele <gabri.cabal@gmail.com>
2020-10-24 21:55:07 +0200

    Aggiunta specializzazione GArray per gestire le finestre

---
+++ GWindowArray.h
@@ -0,0 +1,17 @@
+/*
+
+Rappresenta un array di finestre come specificazione del template di GArray
+*/
+
+#ifndef GWINARR_H
+
+#include "GArray.h"
+#include "GWindow.h"
+
+/// Rappresenta un array finestre
+class GWindowArray : public GArray<GWindow, 10>{
+public:
+    
+};
+
+#endif
