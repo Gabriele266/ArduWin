d2abb7aaf71681b805627039fb199fd19a76cfc6
Gabriele <gabri.cabal@gmail.com>
2020-10-24 10:10:40 +0200

    Implementazione flag di esecuzione

---
+++ flags.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Gabriele on 24/10/2020.
+// Contiene le informazioni di esecuzione della libreria arduwin
+
+#ifndef FLAGS_CPP
+
+#define FLAGS_CPP
+
+// Indica alla libreria di mostrare errori seriali in caso succedessero
+#define ENABLE_SERIAL_ERRORS
+
+#endif
